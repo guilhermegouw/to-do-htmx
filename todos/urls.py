9f6808eab26c68d6ef9f1f6cb71952393615efd1
guilhermegouw <guilherme.gouw@gmail.com>
2024-07-12 06:52:02 -0300

    Fixed nested navbars + implemented search

--- todos/urls.py
+++ todos/urls.py
@@ -14,12 +14,12 @@ urlpatterns = [
     path("", todo_list, name="todo_list"),
     path("add/", add_todo, name="add_todo"),
     path("add/form/", add_todo_form, name="add_todo_form"),
+    path("edit_form/<int:todo_id>/", edit_todo_form, name="edit_todo_form"),
+    path("edit/<int:todo_id>/", edit_todo, name="edit_todo"),
     path(
         "mark_complete/<int:todo_id>/",
         mark_todo_complete,
         name="mark_todo_complete",
     ),
     path("delete/<int:todo_id>/", delete_todo, name="delete_todo"),
-    path("edit_form/<int:todo_id>/", edit_todo_form, name="edit_todo_form"),
-    path("edit/<int:todo_id>/", edit_todo, name="edit_todo"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -4,13 +4,25 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .models import Todo
 
 
+def is_htmx(request):
+    return request.headers.get("HX-Request") == "true"
+
+
 def todo_list(request):
-    todos = Todo.objects.all()
+    query = request.GET.get("query")
+    if query:
+        todos = Todo.objects.filter(title__icontains=query)
+    else:
+        todos = Todo.objects.all()
+    if is_htmx(request):
+        return render(
+            request, "todos/todo_list_partial.html", {"todos": todos}
+        )
     return render(request, "todos/todo_list.html", {"todos": todos})
 
 
 def add_todo_form(request):
-    return render(request, "todos/add_todo_form.html")
+    return render(request, "todos/add_todo_form_partial.html")
 
 
 def add_todo(request):
@@ -18,6 +30,11 @@ def add_todo(request):
         title = request.POST.get("title")
         description = request.POST.get("description")
         Todo.objects.create(title=title, description=description)
+        if is_htmx(request):
+            todos = Todo.objects.all()
+            return render(
+                request, "todos/todo_list_partial.html", {"todos": todos}
+            )
         return redirect("todo_list")
     return HttpResponse(status=400)
 
@@ -26,12 +43,22 @@ def mark_todo_complete(request, todo_id):
     todo = get_object_or_404(Todo, id=todo_id)
     todo.completed = not todo.completed
     todo.save()
+    if is_htmx(request):
+        todos = Todo.objects.all()
+        return render(
+            request, "todos/todo_list_partial.html", {"todos": todos}
+        )
     return redirect("todo_list")
 
 
 def delete_todo(request, todo_id):
     todo = get_object_or_404(Todo, id=todo_id)
     todo.delete()
+    if is_htmx(request):
+        todos = Todo.objects.all()
+        return render(
+            request, "todos/todo_list_partial.html", {"todos": todos}
+        )
     return redirect("todo_list")
 
 
@@ -43,10 +70,15 @@ def edit_todo(request, todo_id):
         todo.title = title
         todo.description = description
         todo.save()
+        if is_htmx(request):
+            todos = Todo.objects.all()
+            return render(
+                request, "todos/todo_list_partial.html", {"todos": todos}
+            )
         return redirect("todo_list")
     return HttpResponse(status=400)
 
 
 def edit_todo_form(request, todo_id):
     todo = get_object_or_404(Todo, id=todo_id)
-    return render(request, "todos/edit_todo_form.html", {"todo": todo})
+    return render(request, "todos/edit_todo_form_partial.html", {"todo": todo})
